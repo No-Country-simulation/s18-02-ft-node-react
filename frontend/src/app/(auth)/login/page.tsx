c84a981be68e902a9e190ab2cce3b413b45ffdb5
David Garcia Amador <87836906+dagamdev@users.noreply.github.com>
2024-10-12 18:56:39 +0000

    Add logo component

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,7 +1,11 @@
 import LoginForm from '@/components/login-form'
+import Logo from '@/components/logo'
 
 export default function LoginPage () {
   return (
-    <LoginForm />
+    <section className='max-w-sm mx-auto flex flex-col gap-y-16 items-center pt-8'>
+      <Logo />
+      <LoginForm />
+    </section>
   )
 }
--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,10 +1,12 @@
+import Logo from '@/components/logo'
 import RegisterForm from '@/components/register-form'
 
 export default function LoginPage () {
   return (
-    <section className='max-w-md space-y-8 mx-auto'>
+    <section className='max-w-md space-y-8 mx-auto pt-2'>
       <div>
-        <h1 className='font-bold text-xl'>Registra tu usuario</h1>
+        <Logo size={10} />
+        <h1 className='font-bold text-xl mt-2'>Registra tu usuario</h1>
         <p className='text-sm text-muted-foreground'>Descripcion para la seccion del resgitro de usuario ya sea de tipo alumno o profesor.</p>
       </div>
       <RegisterForm />
