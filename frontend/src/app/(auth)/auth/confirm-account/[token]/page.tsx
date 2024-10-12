dd858f64594e2554e77952a7c30f3cc34d740a90
David Garcia Amador <87836906+dagamdev@users.noreply.github.com>
2024-10-12 22:51:14 +0000

    Add auth confirm account page

---
+++ frontend/src/app/(auth)/auth/confirm-account/[token]/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Logo from '@/components/logo'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import api from '@/lib/api'
+
+export default function AuthConfirmAccountPage ({ params: { token } }: { params: { token: string } }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    api.confirmEmail(token).then(res => {
+      console.log(res)
+      router.push('/login')
+    }).catch(console.error)
+  }, [token, router])
+
+  return (
+    <section className='max-w-sm mx-auto flex flex-col items-center space-y-12 pt-10'>
+      <Logo />
+      <h1 className='font-semibold text-xl'>Confirmamos tu email con éxito!</h1>
+    </section>
+  )
+}
