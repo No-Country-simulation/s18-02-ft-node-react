4cad15f713134e4acdb7a11e5df415750760c3d9
David Garcia Amador <87836906+dagamdev@users.noreply.github.com>
2024-10-12 22:51:48 +0000

    Get current

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,15 @@
+'use client'
+
+import api from '@/lib/api'
+import { useEffect } from 'react'
+
 export default function Home () {
+  useEffect(() => {
+    api.current().then(res => {
+      console.log(res)
+    }).catch(console.error)
+  }, [])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Hello
