b719bb844a201a57d8a89d1d559dea27281a287d
David Garcia Amador <87836906+dagamdev@users.noreply.github.com>
2024-10-14 02:41:16 +0000

    Add PostCard componet

---
+++ frontend/src/components/post-card.tsx
@@ -0,0 +1,31 @@
+import { Badge } from './ui/badge'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
+
+export default function PostCard ({ user }: { user: User }) {
+  return (
+    <Card>
+      <CardHeader>
+        {user.avatar !== undefined && <img src={user.avatar} alt={`Avatar of ${user.username}`} />}
+        <div>
+          <CardTitle>Card Title</CardTitle>
+          {/* rating */}
+        </div>
+      </CardHeader>
+      <CardContent className='space-y-2'>
+        {user.subjects !== undefined && <ul
+          className='flex gap-2'
+        >
+          {user.subjects.map(subject => <Badge key={subject} variant='outline'>{subject.toUpperCase()}</Badge>)}
+        </ul>}
+        {user.description !== undefined && <CardDescription>{user.description}</CardDescription>}
+      </CardContent>
+      <CardFooter className='flex justify-between'>
+        <span className='font-bold text-xl'>$99/h</span>
+        <Badge
+          variant='outline'
+          className='border-green-500 text-green-500'
+        >MODO VIRTUAL</Badge>
+      </CardFooter>
+    </Card>
+  )
+}
