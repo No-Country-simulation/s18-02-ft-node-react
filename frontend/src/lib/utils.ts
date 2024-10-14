86dd55ffc018fa6ace0530528c16eae84766b30c
David Garcia Amador <87836906+dagamdev@users.noreply.github.com>
2024-10-14 18:23:35 +0000

    Add getNameInitials method

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn (...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getNameInitials (name: string) {
+  return name.split(/ +/g).map(s => s[0]).slice(0, 3).join('')
+}
