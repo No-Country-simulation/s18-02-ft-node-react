4710205a8ac372cec0a2b762449d765ba1e54bff
gacub20ru <gacub20ru@gmail.com>
2024-10-16 18:22:05 +0000

    Add svg icon component

---
+++ frontend/src/icons/adjustments.tsx
@@ -0,0 +1,20 @@
+export default function AdjustmentsIcon ({ ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      {...props}
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M8 4V2.667M8 4a1.333 1.333 0 1 0 0 2.667M8 4a1.333 1.333 0 1 1 0 2.667m0 0v6.666M4 12a1.334 1.334 0 0 0 0-2.667M4 12a1.334 1.334 0 0 1 0-2.667M4 12v1.333m0-4V2.667M12 12a1.333 1.333 0 0 0 0-2.667M12 12a1.334 1.334 0 0 1 0-2.667M12 12v1.333m0-4V2.667'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
