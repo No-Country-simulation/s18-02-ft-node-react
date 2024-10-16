faa7200a353acbef97b5fc18b651a599283ab58d
gacub20ru <gacub20ru@gmail.com>
2024-10-16 23:18:32 +0000

    Add SearchBar component

---
+++ frontend/src/components/search-bar.tsx
@@ -0,0 +1,24 @@
+import SearchIcon from '@/icons/search'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import AdjustmentsIcon from '@/icons/adjustments'
+
+export default function SearchBar () {
+  return (
+    <form
+      className='bg-slate-600 flex gap-x-2 px-5 py-8'
+      onSubmit={e => {
+        e.preventDefault()
+        console.log('search')
+      }}
+    >
+      <Input type='search' placeholder='Buscar' />
+      <Button size='icon' className='p-2'>
+        <SearchIcon />
+      </Button>
+      <Button size='icon' variant='outline' className='p-2 bg-transparent'>
+        <AdjustmentsIcon />
+      </Button>
+    </form>
+  )
+}
