62daa1735d4ba73f42f88148499cdf5a03fb5a6d
gacub20ru <gacub20ru@gmail.com>
2024-10-19 20:23:45 +0000

    Add basic middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import api from './lib/api'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware (request: NextRequest) {
+  const token = request.cookies.get('token')
+  console.log(token)
+
+  if (token === undefined) return NextResponse.redirect(new URL('/login', request.url))
+
+  // try {
+  //   const user = await api.current()
+  //   console.log(user)
+  // } catch (error) {
+  //   console.error(error)
+  //   return NextResponse.redirect(new URL('/login', request.url))
+  // }
+
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/profile/:path*'
+}
