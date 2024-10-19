8e148839a48adb549868fbeee81a89967bfbbd8c
gacub20ru <gacub20ru@gmail.com>
2024-10-19 20:26:57 +0000

    Add main route group

--- frontend/src/app/(main)/profile/[username]/page.tsx
+++ frontend/src/app/(main)/profile/[username]/page.tsx
@@ -28,9 +28,6 @@ export default async function ProfilePage ({ params: { username } }: { params: {
 
   console.log(user)
 
-  const isMyProfile = user.id === sessionUser.id
-  const isTeacher = user.role === 'teacher'
-
   return (
     <main className='flex-1 py-6 px-5 space-y-6'>
       <section className='flex gap-x-6 items-start'>
