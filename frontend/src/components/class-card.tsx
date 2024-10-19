774d9288c64225bcbb8b32babf236b15077c71bb
gacub20ru <gacub20ru@gmail.com>
2024-10-19 20:27:16 +0000

    Initial class card

--- frontend/src/components/class-card.tsx
+++ frontend/src/components/class-card.tsx
@@ -1,27 +1,9 @@
-import RatingStars from './rating-stars'
-import ClassModeBadge from './shared/class-mode-badge'
-import SubjectsList from './subjects-list'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
+import { Card } from './ui/card'
 
-export default function ClassCard ({ user }: { user: TeacherUser }) {
+export default function ClassCard () {
   return (
-    <Card className='max-w-sm rounded-lg'>
-      {user.avatar !== undefined && <img className='rounded-lg w-full' src={user.avatar} alt={`Avatar of ${user.username}`} />}
-      <CardHeader className='flex-row justify-between items-center'>
-        <CardTitle>Card Title</CardTitle>
-        <RatingStars rating={3.5} />
-      </CardHeader>
-      <CardContent className='space-y-2'>
-        {user.subjects !== undefined && <SubjectsList subjects={user.subjects}/>}
-        {user.description !== undefined && <CardDescription>{user.description}</CardDescription>}
-      </CardContent>
-      <CardFooter className='flex justify-between'>
-        <span className='font-bold text-2xl'>$99/h</span>
-        <div className='flex gap-x-2'>
-          <p>Disponible 04/07</p>
-          <ClassModeBadge classMode={user.classMode}/>
-        </div>
-      </CardFooter>
+    <Card>
+      hola
     </Card>
   )
 }
