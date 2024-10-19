7fbfae57554f6936c3f2136ce79b2aa340b45689
gacub20ru <gacub20ru@gmail.com>
2024-10-19 20:23:35 +0000

    Add EmojiSad svg icon

---
+++ frontend/src/icons/emoji-sad.tsx
@@ -0,0 +1,19 @@
+export default function EmojiSadIcon ({ ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      {...props}
+      width='156'
+      height='155'
+      viewBox='0 0 156 155'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M78 139.5a62.003 62.003 0 0 0 62-62 62 62 0 1 0-62 62ZM54.75 69.75a7.75 7.75 0 1 0 0-15.5 7.75 7.75 0 0 0 0 15.5ZM109 62a7.752 7.752 0 0 1-7.75 7.75A7.75 7.75 0 1 1 109 62Zm-58.404 45.562a7.743 7.743 0 0 0 5.483 2.273 7.75 7.75 0 0 0 5.483-2.273 23.252 23.252 0 0 1 32.876 0 7.755 7.755 0 0 0 10.966-10.966 38.744 38.744 0 0 0-42.235-8.403 38.749 38.749 0 0 0-12.573 8.403 7.75 7.75 0 0 0 0 10.966Z'
+        fill='currentColor'
+      />
+    </svg>
+  )
+}
