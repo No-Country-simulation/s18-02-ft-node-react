6e9eef7da5dca8a42ba3a1d49b380fdb4d0b00fb
gacub20ru <gacub20ru@gmail.com>
2024-10-20 02:16:20 +0000

    Update styles

--- frontend/src/app/(main)/profile/[username]/layout.tsx
+++ frontend/src/app/(main)/profile/[username]/layout.tsx
@@ -1,13 +1,9 @@
-import Navbar from '@/components/shared/navbar'
 import React from 'react'
 
 export default function ProfileLayout ({ children }: { children: React.ReactNode }) {
   return (
-    <div className='min-h-dvh flex flex-col'>
-      <main className='flex-1 px-5 py-6 space-y-6'>
-        {children}
-      </main>
-      <Navbar />
-    </div>
+    <main className='flex flex-col gap-y-6 flex-1 px-5 py-6'>
+      {children}
+    </main>
   )
 }
