bc1747701b430f04d3375ca57c333e82160f3b13
gacub20ru <gacub20ru@gmail.com>
2024-10-23 20:13:41 +0000

    Add active style in navbar link

--- frontend/src/components/shared/navbar.tsx
+++ frontend/src/components/shared/navbar.tsx
@@ -6,9 +6,12 @@ import CalendarIcon from '@/icons/calendar'
 import AcademicCapIcon from '@/icons/academic-cap'
 import PencilAltIcon from '@/icons/pencil-alt'
 import { useSessionStore } from '@/stores/session'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 export default function Navbar () {
   const sessionUser = useSessionStore(store => store.user)
+  const pathname = usePathname()
 
   if (sessionUser === undefined) return null
 
@@ -39,22 +42,31 @@ export default function Navbar () {
     })
   }
 
-  console.log(links, sessionUser)
-
   return (
     <nav className='sticky bottom-0 flex py-4 px-8 justify-between bg-secondary'>
-      {links.map(link => <Link
-        key={link.name}
-        href={link.path}
-        className='flex flex-col items-center gap-y-1 text-[#607D8B]'
-      >
-        <span className='bg-background p-3 rounded-md flex'>
-          <link.icon />
-        </span>
-        <span className='text-xs text-center'>
-          {link.name}
-        </span>
-      </Link>)}
+      {links.map(link => {
+        let active = false
+
+        if (link.path === '/') {
+          active = pathname === '/'
+        } else active = pathname.startsWith(link.path)
+
+        return (<Link
+          key={link.name}
+          href={link.path}
+          className='flex flex-col items-center gap-y-1 text-[#607D8B]'
+        >
+          <span className={cn('bg-background p-3 rounded-md flex', active
+            ? 'bg-[#607D8B] text-white'
+            : ''
+          )}>
+            <link.icon />
+          </span>
+          <span className='text-xs text-center'>
+            {link.name}
+          </span>
+        </Link>)
+      })}
     </nav>
   )
 }
