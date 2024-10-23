c21f20274f147a5e478dd7c1b584667ec85297d5
gacub20ru <gacub20ru@gmail.com>
2024-10-23 19:57:17 +0000

    Fix error in axios config

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,10 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import { type AxiosInstance } from 'axios'
 import type { LoginSchema, RegisterSchema } from './zod'
 import { API_URL } from './constants'
-
-axios.defaults.baseURL = API_URL
-axios.defaults.headers.post['Content-Type'] = 'application/json'
-axios.defaults.headers.patch['Content-Type'] = 'application/json'
 
 interface BaseResponse<T> {
   payload: T
@@ -15,19 +11,50 @@ interface LoginResponse extends BaseResponse<SessionUser> {
   token: string
 }
 
-export function createApiMethods (authInstance: AxiosInstance) {
+const authRoutes = ['current', 'profile']
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:276667114.
+export function createApiMethods (axiosInstance: AxiosInstance, getToken: () => string | undefined) {
+  axiosInstance.defaults.baseURL = API_URL
+  axiosInstance.defaults.headers.post['Content-Type'] = 'application/json'
+  axiosInstance.defaults.headers.put['Content-Type'] = 'application/json'
+
+  axiosInstance.interceptors.request.use(config => {
+    console.log('Axios request')
+
+    console.log(config)
+    if (authRoutes.some(route => config.url?.includes(route))) {
+      const token = getToken()
+
+      if (token !== undefined) config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  }, async error => {
+    console.error('Request interceptor error: ', error.response)
+    return Promise.reject(error)
+  })
+
+  axiosInstance.interceptors.response.use(response => {
+    console.log('Response interceptor: ')
+    return response.data
+  }, async error => {
+    console.error('Response interceptor error: ', error.response)
+    return Promise.reject(error)
+  })
+
   return {
     async login (credentials: LoginSchema): Promise<LoginResponse> {
-      return await axios.post('auth/login', credentials)
+      return await axiosInstance.post('auth/login', credentials)
     },
     async register (user: RegisterSchema & { role: User['role'] }) {
-      return axios.post('auth/register', { ...user, repassword: user.repeatedPassword })
+      return axiosInstance.post('auth/register', { ...user, repassword: user.repeatedPassword })
     },
     async confirmEmail (token: string) {
-      return axios.post(`auth/confirm-email/${token}`)
+      return axiosInstance.post(`auth/confirm-email/${token}`)
     },
     async current (): Promise<BaseResponse<SessionUser>> {
-      return authInstance.get('auth/current')
+      return axiosInstance.get('auth/current')
     }
   }
 }
