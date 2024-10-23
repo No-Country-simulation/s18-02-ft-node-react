8b354723db6d8d83693205602896828591d2acee
gacub20ru <gacub20ru@gmail.com>
2024-10-23 19:57:17 +0000

    Fix error in axios config

--- frontend/src/lib/client/api.ts
+++ frontend/src/lib/client/api.ts
@@ -2,27 +2,7 @@ import axios from 'axios'
 import { getTokenFromClient } from '.'
 import { createApiMethods } from '../axios'
 
-export const authInstance = axios.create()
-
-axios.interceptors.request.use(config => {
-  const token = getTokenFromClient()
-
-  if (token !== undefined) config.headers.Authorization = `Bearer ${token}`
-
-  return config
-}, error => {
-  console.error('Request interceptor error: ', error.response)
-  return error.response.data
-})
-
-axios.interceptors.response.use(response => {
-  console.log('Response interceptor: ', response)
-  return response.data
-}, async error => {
-  console.error('Response interceptor error: ', error.response)
-  return Promise.reject(error)
-})
-
-const api = createApiMethods(authInstance)
+const clientInstance = axios.create()
+const api = createApiMethods(clientInstance, getTokenFromClient)
 
 export default api
--- frontend/src/lib/server/api.ts
+++ frontend/src/lib/server/api.ts
@@ -2,28 +2,7 @@ import axios from 'axios'
 import { getTokenFromServer } from '.'
 import { createApiMethods } from '../axios'
 
-export const authInstance = axios.create()
-
-authInstance.interceptors.request.use(config => {
-  console.log('Server request')
-  const token = getTokenFromServer()
-
-  if (token !== undefined) config.headers.Authorization = `Bearer ${token}`
-
-  return config
-}, async error => {
-  console.error('SV Request interceptor error: ', error.response)
-  return Promise.reject(error)
-})
-
-authInstance.interceptors.response.use(response => {
-  console.log('SV Response interceptor: ')
-  return response.data
-}, async error => {
-  console.error('SV Response interceptor error: ', error.response)
-  return Promise.reject(error)
-})
-
-const api = createApiMethods(authInstance)
+const serverInstance = axios.create()
+const api = createApiMethods(serverInstance, getTokenFromServer)
 
 export default api
