37a6db1076bbfd2775794001e32f6f1197534d85
gacub20ru <gacub20ru@gmail.com>
2024-10-23 00:46:34 +0000

    Use createApiMethods

--- frontend/src/lib/client/api.ts
+++ frontend/src/lib/client/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { API_URL } from '@/lib/constants'
-import { type RegisterSchema, type LoginSchema } from '../zod'
 import { getTokenFromClient } from '.'
+import { createApiMethods } from '../axios'
 
 axios.defaults.baseURL = API_URL
 axios.defaults.headers.post['Content-Type'] = 'application/json'
@@ -27,29 +27,6 @@ authInstance.interceptors.response.use(response => {
   return Promise.reject(error)
 })
 
-const api = {
-  async login (credentials: LoginSchema) {
-    return await axios.post<{
-      payload: User
-      token: string
-    }>('auth/login', credentials)
-  },
-  async register (user: RegisterSchema & { role: User['role'] }) {
-    return axios.post('auth/register', { ...user, repassword: user.repeatedPassword })
-  },
-  async confirmEmail (token: string) {
-    return axios.post(`auth/confirm-email/${token}`)
-  },
-  async current (token?: string) {
-    return authInstance.get<{
-      payload: SessionUser
-      status: string
-    }>('auth/current', {
-      headers: {
-        Authorization: token === undefined ? undefined : `Bearer ${token}`
-      }
-    })
-  }
-}
+const api = createApiMethods(axios, authInstance)
 
 export default api
