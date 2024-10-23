6aff5597c995f2216f4836f24120f3d1cea780b5
gacub20ru <gacub20ru@gmail.com>
2024-10-23 19:56:57 +0000

    Add session loader

---
+++ frontend/src/components/shared/load-session.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import api from '@/lib/client/api'
+import { useSessionStore } from '@/stores/session'
+import { useEffect } from 'react'
+
+export default function LoadSession () {
+  const setSession = useSessionStore(store => store.setSession)
+
+  useEffect(() => {
+    api.current().then(res => {
+      setSession(res.payload)
+    }).catch(error => {
+      console.log('error in load session: ', error)
+    })
+  }, [setSession])
+
+  return null
+}
