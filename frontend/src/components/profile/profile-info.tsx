0f654bbe49a48572f68ef87ba4958d75bea7a52f
gacub20ru <gacub20ru@gmail.com>
2024-10-23 00:47:17 +0000

    Add base ProfileInfo component

--- frontend/src/components/profile/profile-info.tsx
+++ frontend/src/components/profile/profile-info.tsx
@@ -2,7 +2,6 @@
 
 import { useSessionStore } from '@/stores/session'
 import { notFound } from 'next/navigation'
-import { Button } from '../ui/button'
 
 export default function ProfileInfo ({ user }: { user: User }) {
   const sessionUser = useSessionStore(store => store.user)
@@ -12,21 +11,11 @@ export default function ProfileInfo ({ user }: { user: User }) {
   }
 
   return (
-    <div className='flex flex-col justify-between flex-1 gap-y-4'>
+    <>
       <section>
         <h2 className='font-bold text-sm mb-1'>Sobre mí</h2>
         <p className='text-neutral-500'>{user.description}</p>
       </section>
-
-      {(user.role === 'teacher' && sessionUser.id !== user.id) && <section
-        className='flex gap-x-4'
-      >
-        <Button
-          variant='outline'
-          className='w-full'
-        >Añadir a Mis profesores</Button>
-        <Button className='w-full'>Reserva una clase</Button>
-      </section>}
-    </div>
+    </>
   )
 }
