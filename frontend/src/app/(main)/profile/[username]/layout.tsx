ca69e09720f429838ed08077b5b8830c592f3d32
gacub20ru <gacub20ru@gmail.com>
2024-10-23 00:28:08 +0000

    Remove profile layout

--- frontend/src/app/(main)/profile/[username]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProfileLayout ({ children }: { children: React.ReactNode }) {
-  return (
-    <main className='flex flex-col gap-y-6 flex-1 px-5 py-6'>
-      {children}
-    </main>
-  )
-}
