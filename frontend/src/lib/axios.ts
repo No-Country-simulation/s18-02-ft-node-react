53f1a5c254fddcf121e7909031bcdbf6e07ec424
gacub20ru <gacub20ru@gmail.com>
2024-10-23 00:49:18 +0000

    Add SessionUserResponse interface

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,13 +1,15 @@
 import { type AxiosInstance } from 'axios'
 import type { LoginSchema, RegisterSchema } from './zod'
 
+interface SessionUserResponse {
+  payload: SessionUser
+  status: string
+}
+
 export function createApiMethods (baseInstance: AxiosInstance, authInstance: AxiosInstance) {
   return {
-    async login (credentials: LoginSchema) {
-      return await baseInstance.post<{
-        payload: User
-        token: string
-      }>('auth/login', credentials)
+    async login (credentials: LoginSchema): Promise<SessionUserResponse> {
+      return await baseInstance.post('auth/login', credentials)
     },
     async register (user: RegisterSchema & { role: User['role'] }) {
       return baseInstance.post('auth/register', { ...user, repassword: user.repeatedPassword })
@@ -15,11 +17,8 @@ export function createApiMethods (baseInstance: AxiosInstance, authInstance: Axi
     async confirmEmail (token: string) {
       return baseInstance.post(`auth/confirm-email/${token}`)
     },
-    async current () {
-      return authInstance.get<{
-        payload: SessionUser
-        status: string
-      }>('auth/current')
+    async current (): Promise<SessionUserResponse> {
+      return authInstance.get('auth/current')
     }
   }
 }
