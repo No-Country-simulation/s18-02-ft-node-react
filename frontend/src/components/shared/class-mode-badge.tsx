842d1bdc8b0d25f5391032b5110301835dc2d69d
gacub20ru <gacub20ru@gmail.com>
2024-10-24 18:51:10 +0000

    Add presencial style

--- frontend/src/components/shared/class-mode-badge.tsx
+++ frontend/src/components/shared/class-mode-badge.tsx
@@ -1,10 +1,11 @@
+import { cn } from '@/lib/utils'
 import { Badge } from '../ui/badge'
 
-export default function ClassModeBadge ({ classMode }: { classMode: string }) {
+export default function ClassModeBadge ({ classMode }: { classMode: TeacherUser['classMode'] }) {
   return (
     <Badge
       variant='outline'
-      className='border-green-500 text-green-500 rounded-md py-1'
+      className={cn('rounded-md py-1', classMode === 'presencial' ? 'bg-yellow-500 text-yellow-500' : 'bg-green-500 text-green-500')}
     >{classMode.toUpperCase()}</Badge>
   )
 }
