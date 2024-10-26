7081073ef35eb76ff89dae344daa1bfb1a675d42
gacub20ru <gacub20ru@gmail.com>
2024-10-26 01:16:23 +0000

    Init availability route

--- frontend/src/app/(main)/profile/[username]/availability/page.tsx
+++ frontend/src/app/(main)/profile/[username]/availability/page.tsx
@@ -1,5 +1,18 @@
-export default function AvailabilityPage () {
+import AvailabilityManager from '@/components/profile/availability-manager'
+import api from '@/lib/server/api'
+
+export default async function AvailabilityPage ({ params: { username } }: { params: { username: string } }) {
+  console.log(username)
+
+  const userRes = await api.getMyProfile()
+  const user = userRes.payload
+  console.log(user)
+
   return (
-    <p>En esta pagina se configuraran los horarios de disponibilidad del profesor en el mes, ruta solo disponible para el profesor</p>
+    <main className='flex-1 flex flex-col gap-y-6 py-6 px-4 bg-secondary'>
+      <h1 className='text-2xl font-bold'>Disponibilidad del mes</h1>
+
+      <AvailabilityManager user={user as TeacherUser} />
+    </main>
   )
 }
