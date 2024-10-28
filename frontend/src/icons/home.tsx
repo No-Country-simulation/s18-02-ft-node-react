942319b6f0834aa20175d813fbe3b460712f25b3
gacub20ru <gacub20ru@gmail.com>
2024-10-28 03:53:18 +0000

    Fix sizes

--- frontend/src/icons/home.tsx
+++ frontend/src/icons/home.tsx
@@ -3,8 +3,8 @@ export default function HomeIcon ({ ...props }: React.ComponentProps<'svg'>) {
     <svg
       {...props}
       width='20'
-      height='21'
-      viewBox='0 0 20 21'
+      height='20'
+      viewBox='0 0 20 20'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
