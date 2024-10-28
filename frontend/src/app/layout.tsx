957c6cec33328bc25c3d7d29cc0b3f258dff9b83
gacub20ru <gacub20ru@gmail.com>
2024-10-28 23:15:19 +0000

    Add suppressHydrationWarning

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout ({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="es" className='light' style={{ colorScheme: 'light' }}>
+    <html lang="es" suppressHydrationWarning>
       <head>
         <meta name="viewport" content= "width=device-width, user-scalable=no" />
       </head>
