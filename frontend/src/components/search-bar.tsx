3a0890808a62fa7ff58e615e42758611a4896a8a
gacub20ru <gacub20ru@gmail.com>
2024-10-28 22:02:24 +0000

    Update styles

--- frontend/src/components/search-bar.tsx
+++ frontend/src/components/search-bar.tsx
@@ -4,11 +4,14 @@ import SearchIcon from '@/icons/search'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import AdjustmentsIcon from '@/icons/adjustments'
+import { useSessionStore } from '@/stores/session'
 
 export default function SearchBar () {
+  const loged = useSessionStore(store => store.user !== undefined)
+
   return (
     <form
-      className='bg-border flex gap-x-2 px-5 py-8'
+      className={'flex gap-x-2 px-5 py-8 ' + (loged ? 'bg-primary' : 'bg-secondary')}
       onSubmit={e => {
         e.preventDefault()
         console.log('search')
