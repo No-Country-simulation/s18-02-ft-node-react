316c4e7e542ea806df851defe8bce4441438cb19
Abimael Junior Pacheco <122593014+Abimael-p@users.noreply.github.com>
2024-10-30 17:20:04 +0000

    rute/classes

---
+++ frontend/src/icons/next-button-icon.tsx
@@ -0,0 +1,20 @@
+export default function NextButtonIcon({ ...props }: React.ComponentProps<'svg'>) {
+    return (
+        <svg
+            {...props}
+            width="15"
+            height="14"
+            viewBox="0 0 15 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M6 2.91667L10.0833 7L6 11.0833"
+                stroke="#607D8B"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
