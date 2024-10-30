9335064ea094c601c2b3ce15d33eb3e118f57d67
gacub20ru <gacub20ru@gmail.com>
2024-10-30 00:20:49 +0000

    Fix build errors

--- frontend/src/app/(main)/classes/[id]/page.tsx
+++ frontend/src/app/(main)/classes/[id]/page.tsx
@@ -1,4 +1,5 @@
-export default function Page ({ props: { id } }: { props: { id: string } }) {
+export default function Page ({ params: { id } }: { params: { id: string } }) {
+  console.log(id)
   // id es el id de la clase
   return (
     <p>Muestra mas detalles de la clase reservada que tienes ya ses alumno o maestro</p>
