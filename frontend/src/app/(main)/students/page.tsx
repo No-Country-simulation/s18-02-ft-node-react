680648e7042fd208bd8dc12c516e5ab4a3842ee9
gacub20ru <gacub20ru@gmail.com>
2024-10-30 19:32:19 +0000

    Update components

--- frontend/src/app/(main)/students/page.tsx
+++ frontend/src/app/(main)/students/page.tsx
@@ -1,5 +1,8 @@
 export default function StudentsPage () {
   return (
-    <p>Solo tendra acceso el profesor y vera sus estudiantes</p>
+    <main className='flex-1 flex flex-col gap-y-6 py-6 px-4'>
+      <h1>En desarrollo</h1>
+      <p>Solo tendra acceso el profesor y vera sus estudiantes</p>
+    </main>
   )
 }
--- frontend/src/app/(main)/teachers/page.tsx
+++ frontend/src/app/(main)/teachers/page.tsx
@@ -1,5 +1,8 @@
 export default function TeachersPage () {
   return (
-    <p>Esta ruta solo tendra acceso el alumno y se le mostrara sus maestros</p>
+    <main className='flex-1 flex flex-col gap-y-6 py-6 px-4'>
+      <h1>En desarrollo</h1>
+      <p>Esta ruta solo tendra acceso el alumno y se le mostrara sus maestros</p>
+    </main>
   )
 }
