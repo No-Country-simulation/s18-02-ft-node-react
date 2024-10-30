a41053e4eb5896ddde3a0b980e1a836747470826
gacub20ru <gacub20ru@gmail.com>
2024-10-30 21:21:51 +0000

    Update

--- frontend/src/components/shared/class-schedule-manager.tsx
+++ frontend/src/components/shared/class-schedule-manager.tsx
@@ -6,10 +6,9 @@ import { Button } from '../ui/button'
 import { cn } from '@/lib/utils'
 import XIcon from '@/icons/x'
 
-const schedules = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
-
-export function AgendaManager ({ availability }: {
+export function ClassScheduleManager ({ availability, schedules }: {
   availability: TeacherUser['schedulePreferences']
+  schedules: string[]
 }) {
   const [date, setDate] = useState<Date | undefined>(new Date())
   const dateFormated = new Intl.DateTimeFormat('es-ES', {
@@ -21,11 +20,10 @@ export function AgendaManager ({ availability }: {
   const [selectedSchedules] = useState<string[]>(Object.entries(availability)
     .find(([key, value]) => typeof value === 'object' && value.length > 0)?.[1] as string[] ?? []
   )
-
-  console.log(availability, selectedSchedules)
+  const [schedulesEdited, setSchedulesEdited] = useState<string[]>(schedules)
 
   const handleClick = () => {
-    console.log('editando..')
+    setEdit(false)
   }
 
   return (
@@ -44,10 +42,18 @@ export function AgendaManager ({ availability }: {
       <section>
         <h2 className='mb-2 font-semibold'>Disponibilidad horaria</h2>
         <ol className='py-5 px-4 bg-card flex flex-wrap gap-2 border rounded-lg'>
-          {schedules.map(schedule => <li
+          {schedulesEdited.map(schedule => <li
             key={schedule}
             className={cn('border py-1 px-2 rounded font-semibold flex gap-x-1 items-center', selectedSchedules.includes(schedule) ? 'bg-chart-3 text-white border-transparent' : '')}
-          >{schedule}{edit && !selectedSchedules.includes(schedule) ? <XIcon className='size-4 cursor-pointer'/> : ''}</li>)}
+          >{schedule}{edit && !selectedSchedules.includes(schedule)
+            ? <XIcon
+              className='size-4 cursor-pointer'
+              onClick={() => {
+                setSchedulesEdited(scs => scs.filter(s => s !== schedule))
+              }}
+            />
+            : ''
+          }</li>)}
         </ol>
       </section>
 
